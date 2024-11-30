3b457fb30639234679eb79f23ba45cc1ee16dbe0
Zac <zbenattar@gmail.com>
2024-11-30 12:21:07 +0000

    Improve homepage layout

--- src/app/_components/homepage.tsx
+++ src/app/_components/homepage.tsx
@@ -47,46 +47,46 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Pet sitting made easy
-        </h1>
+        <div className="flex flex-row place-content-center">
+          <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Pet sitting made easy
+          </h1>
+        </div>
+
         <div className="flex flex-col gap-8">
           <div className="flex flex-row place-content-center">
-            <span className="text-2xl">
-              Invite friends and family to sit for your pets and organise it all
-              in one place.
+            <span className="text-center text-2xl font-light">
+              Invite <span className="font-medium">friends and family</span> to sit for your pets.
             </span>
           </div>
 
-          <div className="my-5 flex flex-row place-content-center gap-4">
-            <div className="flex w-full max-w-sm items-center space-x-2">
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="w-2/3 space-y-6"
-                >
-                  <FormField
-                    control={form.control}
-                    name="inviteCode"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <div className="flex w-full max-w-sm items-center space-x-2">
-                            <Input
-                              type="text"
-                              placeholder="invite code"
-                              {...field}
-                            />
-                            <Button type="submit">Join Group</Button>
-                          </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </form>
-              </Form>
-            </div>
+          <div className="pt-5 pb-20 flex flex-row place-content-center gap-4">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-2/3 space-y-6"
+              >
+                <FormField
+                  control={form.control}
+                  name="inviteCode"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <div className="flex w-full max-w-sm items-center space-x-2">
+                          <Input
+                            type="text"
+                            placeholder="Invite code"
+                            {...field}
+                          />
+                          <Button type="submit" className="border h-[40px]">Join Group</Button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
           </div>
         </div>
 
